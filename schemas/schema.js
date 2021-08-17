26e974623bbdf0467830dd924d477a1c3518ef20
Carl Smestad <carlsm@stud.ntnu.no>
2021-08-17 22:09:57 +0200

    remove splashevents from schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -12,5 +12,5 @@ export default createSchema({
   name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([splashEvents, resources]),
+  types: schemaTypes.concat([resources]),
 });
