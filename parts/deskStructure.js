57622ce2de117f60a4967682200bf4f8f96b1e3c
Martine Malmo Knutsen <martimkn@stud.ntnu.no>
2022-02-27 15:52:57 +0100

    added articleschame for ow

--- parts/deskStructure.js
+++ parts/deskStructure.js
@@ -1,24 +1,24 @@
 import S from "@sanity/desk-tool/structure-builder";
 
 const deskStructure = () =>
-  S.list()
-    .title("Content")
-    .items([
-      ...getItems(false),
+    S.list()
+        .title("Content")
+        .items([
+            ...getItems(false),
 
-      S.listItem()
-        .title("Wiki Re-Reborn")
-        .child(S.list().title("Content").items(getItems(true))),
-    ]);
+            S.listItem()
+                .title("Wiki Re-Reborn")
+                .child(S.list().title("Content").items(getItems(true))),
+        ]);
 
 const getItems = (setWikiItems) => {
-  const wikiItems = ["committees", "articles", "portals", "users"];
+    const wikiItems = ["committees", "wiki-articles", "portals", "users"];
 
-  return S.documentTypeListItems().filter((listItem) =>
-    setWikiItems
-      ? wikiItems.includes(listItem.getSchemaType().name)
-      : !wikiItems.includes(listItem.getSchemaType().name)
-  );
+    return S.documentTypeListItems().filter((listItem) =>
+        setWikiItems
+            ? wikiItems.includes(listItem.getSchemaType().name)
+            : !wikiItems.includes(listItem.getSchemaType().name)
+    );
 };
 
 export default deskStructure;
---
+++ schemas/articles.js
@@ -0,0 +1,61 @@
+const articleSchema = {
+    name: "article",
+    type: "document",
+    title: "Artikkel",
+    fields: [
+        {
+            name: "title",
+            title: "Tittel",
+            type: "string",
+            validation: (Rule) => Rule.required().error("Krever en tittel"),
+        },
+        { name: "author", title: "Forfatter", type: "string" },
+
+        {
+            name: "tags",
+            title: "Tags",
+            type: "array",
+            of: [{ type: "string" }],
+        },
+        {
+            name: "published_date",
+            title: "Publisert dato",
+            type: "date",
+        },
+        {
+            name: "cover_image",
+            title: "Forsidebilde",
+            type: "image",
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: "slug",
+            title: "slug",
+            type: "slug",
+            options: {
+                source: "title",
+                maxLength: "80",
+            },
+        },
+        { name: "excerpt", title: "Ingress", type: "text" },
+        {
+            name: "content",
+            title: "Innhold",
+            type: "array",
+            of: [
+                { type: "block" },
+                {
+                    type: "image",
+                    options: {
+                        hotspot: true,
+                    },
+                },
+            ],
+        },
+    ],
+    initialValue: { published_date: new Date().toLocaleDateString() },
+};
+
+export default articleSchema;
